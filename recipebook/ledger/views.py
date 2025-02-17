f6f93c66f02b8c2a5faaec45785ec241c4ce798c
Goodymind <aabuke9@gmail.com>
2025-02-17 22:51:01 +0800

    working recipe pages

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .recipes.recipe_parser import parse_recipes
 
+ctx_recipes = {}
+ctx_recipes_loaded = False
+
 def index(request):
     return HttpResponse('index')
 
+def load_recipes():
+    global ctx_recipes
+    global ctx_recipes_loaded
+    if not ctx_recipes_loaded:
+        ctx_recipes = parse_recipes()
+        ctx_recipes_loaded = True
+
 def get_recipe_list(request):
-    return render(request, 'recipe_list.html', parse_recipes())
-# Create your views here.
+    load_recipes()
+    return render(request, 'recipe_list.html', ctx_recipes)
 
 def get_recipe(request):
-
-    return HttpResponse('recipe ' + request.method + request.path)
+    load_recipes()
+    recipes = ctx_recipes['recipes']
+    for recipe in recipes:
+        if recipe['link'] == request.path:
+            return render(request, 'recipe_base.html', recipe)
