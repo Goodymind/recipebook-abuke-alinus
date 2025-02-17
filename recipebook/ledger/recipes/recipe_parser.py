4434e4b8bae1752a9f006a16c36dfaf27f7f2dbb
Goodymind <aabuke9@gmail.com>
2025-02-17 23:31:32 +0800

    recipe_parser clean

--- recipebook/ledger/recipes/recipe_parser.py
+++ recipebook/ledger/recipes/recipe_parser.py
@@ -1,17 +1,27 @@
+'''
+    Module providing a parser for recipes in JSON
+'''
+
 import json
-import os
-# My recipe parser module
 
 PATH = 'ledger/recipes/recipes.json'
 
-
 def parse_recipes():
+    '''
+        gets the recipes from the PATH file and returns it as object
+        dict or list
+    '''
+
     try:
-        with open(PATH, 'r') as recipe_txt:
+        with open(PATH, 'r', encoding='UTF-8') as recipe_txt:
             unparsed_recipe = recipe_txt.read()
             parsed_recipe = json.loads(unparsed_recipe)
             return parsed_recipe
+
     except FileNotFoundError:
         print('Recipes not Found on ' + PATH + ' current directory ')
+
     except json.JSONDecodeError:
         print('Error parsing json text')
+
+    return None
