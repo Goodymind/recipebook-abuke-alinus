24a845fb06c5d5239f42841f8abebbb30bb86c0a
Goodymind <aabuke9@gmail.com>
2025-02-17 14:15:55 +0800

    recipelist_done

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns=[
-    path('', views.index)
+    path('', views.index),
+    path('recipelist', views.get_recipe_list, name="recipe-list-name")
 ]
