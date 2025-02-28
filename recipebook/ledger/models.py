39ea988f60e2f3d61d0f8675885b8c034519ae71
Goodymind <aabuke9@gmail.com>
2025-03-01 03:11:00 +0800

    working models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    unit = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f'{self.name}, units: {self.unit}'
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+
+    def __str__(self):
+        return f'{self.recipe}'
