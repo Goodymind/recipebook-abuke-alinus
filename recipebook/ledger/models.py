e3acf59e2b156295a86d5f61a05214321bf51e07
Goodymind <aabuke9@gmail.com>
2025-03-10 11:39:12 +0800

    moved profile before recipe

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    '''
+        accepts a user, name, and bio
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
 class Recipe(models.Model):
     '''
         accepts name
@@ -31,10 +39,4 @@ class RecipeIngredient(models.Model):
     def __str__(self):
         return f'{self.recipe}'
 
-class Profile(models.Model):
-    '''
-        accepts a user, name, and bio
-    '''
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bio = models.CharField(max_length=255)
+
