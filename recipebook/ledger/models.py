d987e37bab95e8d6f349669654037255db7609d2
Goodymind <aabuke9@gmail.com>
2025-03-10 11:36:33 +0800

    Profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     '''
@@ -29,3 +30,11 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f'{self.recipe}'
+
+class Profile(models.Model):
+    '''
+        accepts a user, name, and bio
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
