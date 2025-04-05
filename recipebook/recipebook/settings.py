7a321614b1b61f751d674ff8cea0a97d997d6271
Goodymind <aabuke9@gmail.com>
2025-04-05 21:23:08 +0800

    added media/images path in settings and urls

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +124,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/recipes/list'
 LOGIN_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_ROOT = BASE_DIR + '/media'
+MEDIA_URL = '/media/'
